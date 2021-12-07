83a28d3701afde2ea41ddb9449860286ed078a26
Fredy Ait <aitfredy@gmail.com>
2021-12-07 11:07:47 +0200

    fixed loop & calculations

--- Day 3/yl3p1.py
+++ Day 3/yl3p1.py
@@ -1,22 +1,28 @@
 gamma = ""
-epsilon = 0
-true = 0
-false = 0
+epsilon = ""
+one = 0
+zero = 0
 char = 0
-num = 0
+
 data = open("Day 3/data.txt").readlines()
 
-for i in data[0]:
+for i in range(len(data[0])-1):
     for line in range(len(data)):
-        if int(data[line][char]) == 1:
-            true += 1
-        else:
-            false += 1
-
-    if true > false:
+        if data[line][char] == "1":
+            one += 1
+        elif data[line][char] == "0":
+            zero += 1
+        
+    if one > zero:
         gamma += "1"
+        epsilon += "0"
     else:
         gamma += "0"
+        epsilon += "1"
     
-    print(gamma)
     char += 1
+    one = 0
+    zero = 0
+
+print("Gamma:", gamma, "Epsilon:", epsilon)
+print(int(gamma, 2), "*", int(epsilon, 2), "=", int(gamma, 2) * int(epsilon, 2))
